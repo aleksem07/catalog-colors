8fd13babe2a8a13f114fe1b936de78864ce4c236
aleksem07 <aleksem07@gmail.com>
2024-07-24 12:48:33 +0300

    feat: install pug

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  rules: [
+    {
+      test: /\.pug$/,
+      oneOf: [
+        {
+          resourceQuery: /^\?vue/,
+          use: ["pug-plain-loader"],
+        },
+        {
+          use: ["raw-loader", "pug-plain-loader"],
+        },
+      ],
+    },
+  ],
 });
